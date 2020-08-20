07c494f1b6e2729759677c01d5a3389c53f15219
Juan Alvarez <juanalvarez@Juans-MacBook-Pro.local>
2020-08-19 23:15:50 -0500

    Fix bug Place food. Substract 1 from grid_width and grid_height

--- src/game.cpp
+++ src/game.cpp
@@ -5,8 +5,8 @@
 Game::Game(std::size_t grid_width, std::size_t grid_height)
     : snake(grid_width, grid_height),
       engine(dev()),
-      random_w(0, static_cast<int>(grid_width)),
-      random_h(0, static_cast<int>(grid_height)) {
+      random_w(0, static_cast<int>(grid_width)-1),
+      random_h(0, static_cast<int>(grid_height)-1) {
   PlaceFood();
 }
 
@@ -18,10 +18,10 @@ void Game::Run(Controller const &controller, Renderer &renderer,
   Uint32 frame_duration;
   int frame_count = 0;
   bool running = true;
-
+  
   while (running) {
+    
     frame_start = SDL_GetTicks();
-
     // Input, Update, Render - the main game loop.
     controller.HandleInput(running, snake);
     Update();
